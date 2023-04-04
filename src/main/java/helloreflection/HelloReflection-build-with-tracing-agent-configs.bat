1b2d60efd161786021aa7a16a19e70da3357423e
王良 <841369634@qq.com>
2023-04-04 11:17:11 +0800

    添加 HelloReflection-build-with-tracing-agent-configs.bat

---
+++ src/main/java/helloreflection/HelloReflection-build-with-tracing-agent-configs.bat
@@ -0,0 +1,14 @@
+title "HelloReflection-Build-with-config"
+
+java -version
+
+javac HelloReflection.java -d ..\..\..\..\target\classes
+
+native-image -classpath ..\..\..\..\target\classes ^
+    helloreflection.HelloReflection ^
+    --no-fallback -H:+PrintImageObjectTree -H:+ExhaustiveHeapScan -H:+PrintAnalysisCallTree ^
+    -H:Path=..\..\..\..\target\exe\helloreflection ^
+    -H:Name=HelloReflection ^
+    -H:ConfigurationFileDirectories=.\tracing-agent\
+
+cmd
